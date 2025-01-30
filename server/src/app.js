eaef2fbdb96d963d48d80f2c4d8bcea9598d55ae
Alexandru <donialexandru@gmail.com>
2025-01-30 23:10:19 +0200

    add router

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,18 @@
 import express from "express";
-
+import router from "./router.js";
+import morgan from "morgan";
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   console.log("hello from my server");
+  res.status(200);
+  res.json({ message: "Hello" });
 });
+
+app.use("/api", router);
 
 export default app;
