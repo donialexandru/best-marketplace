e71c773308d206cec2cc633d5eed51542c3f4186
Alexandru <donialexandru@gmail.com>
2025-02-11 11:07:06 +0200

    add global style.css file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AuthForm from "./components/AuthForm.jsx";
 import Header from "./components/Header.jsx";
-import "./App.css";
 
 const App = () => {
   return (
