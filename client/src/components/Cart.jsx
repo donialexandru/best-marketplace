56e41850bb4b39272860f23e2309029fdc68fc51
Alexandru Doni <donialexandru@gmail.com>
2025-02-23 20:25:35 +0200

    create cart component

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,5 +1,43 @@
-const Cart = () => {
-  return <h2>Welcome to the Cart section</h2>;
+import { useState } from "react";
+import { formatCurrency } from "../utils/currency";
+
+const Cart = ({ cart, checkout }) => {
+  const [loading, setLoading] = useState(true);
+
+  // async function checkout () {
+  //   setLoading(true)
+  //
+  //   await fetch("/api/order", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-type": "application/json",
+  //     },
+  //     body: JSON.stringify({cart}),
+  //   })
+  //
+  //   setCart([])
+  //   setLoading(false)
+  // }
+
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    const current = cart[i];
+    total += Number(current.price);
+  }
+  return (
+    <div className="cart">
+      <h2>Cart</h2>
+      <ul>
+        {cart.map((item, index) => (
+          <li key={index}>
+            <span>{item.name}</span> - <span>{item.price}</span>
+          </li>
+        ))}
+      </ul>
+      <p>Total: {formatCurrency(total)}</p>
+      <button onClick={checkout}>Checkout</button>
+    </div>
+  );
 };
 
 export default Cart;
